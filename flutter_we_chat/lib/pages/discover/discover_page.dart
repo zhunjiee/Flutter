e47d474ec7315b75ece8b9311df2eb4881575f6d
zhunjiee <zhunjiee@qq.com>
2020-03-20 19:46:07 +0800

    分割线终于搞好啦

--- flutter_we_chat/lib/pages/discover/discover_page.dart
+++ flutter_we_chat/lib/pages/discover/discover_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 import '../../widgets/widget_custom_app_bar.dart';
 
 class DiscoverPage extends StatelessWidget {
@@ -7,28 +8,42 @@ class DiscoverPage extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(),
       body: Container(
-        child: _buildDiscoverListTile("images/ic_social_circle.png"),
+        child: _buildDiscoverListTile("images/ic_social_circle.png", true),
       ),
     );
   }
 
-  Widget _buildDiscoverListTile(String src,){
-    return Container(
-      height: 50,
-
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 50,
-            child: Image.asset(src,width: 28, height: 28,),
-          ),
-          Expanded(child: Text("haha")),
-          Container(
-            width: 30,
-            child: Icon(Icons.arrow_forward_ios, color: Colors.grey, size: 15,),
-          ),
-        ],
-      ),
+  Widget _buildDiscoverListTile(
+    String src, bool showSeparateLine
+  ) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              width: 50,
+              height: 50,
+              alignment: Alignment.center,
+              child: Image.asset(src, width: 28, height: 28),
+            ),
+            Expanded(child: Text("haha")),
+            Container(
+              width: 30,
+              child: Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.grey,
+                size: 15,
+              ),
+            ),
+          ],
+        ),
+        // 分割线
+        Container(
+          margin: EdgeInsets.only(left: 50),
+          height: ScreenUtil().setWidth(0.5),
+          color: showSeparateLine ? Colors.grey[400] : Colors.transparent,
+        ),
+      ],
     );
   }
 }
