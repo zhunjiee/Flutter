3dd52670571cd90ccd82185a8080ba0623818fe2
houbaowei <houbaowei@126.com>
2020-03-31 15:49:45 +0800

    短视频

---
+++ project_short_video/lib/pages/index_page.dart
@@ -0,0 +1,9 @@
+/**
+ * @ClassName index_page
+ * @Description 导航页
+ * @Author Houge
+ * @Date 2020/3/31 3:40 PM
+ * @Version 1.0
+ */
+
+import 'package:flutter/material.dart';
---
+++ project_short_video/lib/pages/splash_page.dart
@@ -0,0 +1,9 @@
+/**
+ * @ClassName splash_page
+ * @Description 广告页
+ * @Author Houge
+ * @Date 2020/3/31 3:39 PM
+ * @Version 1.0
+ */
+
+import 'package:flutter/material.dart';
